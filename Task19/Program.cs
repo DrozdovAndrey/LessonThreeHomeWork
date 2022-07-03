98d5de078de5ccd2591b4f7ae1c38192c235b3ba
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-03 16:15:30 +0300

    added task19 for every numbrs

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,6 +7,35 @@
 // 12821 -> да
 
 // 23432 -> да
+// namespace HelloWorld
+
+// {
+//     class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Console.WriteLine("Введите пятизначное число:");
+//             string? number = Convert.ToString(Console.ReadLine());
+           
+            
+
+//             if (number[0] == number[^1])
+//             {
+//                 if (number[01] == number[^2])
+//                 {
+//                    Console.WriteLine("Это полиндром");
+//                 }  
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Это не полиндром");
+//             }
+           
+
+//         }
+//     }
+// }
+
 namespace HelloWorld
 
 {
@@ -15,41 +44,35 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Введите пятизначное число:");
-            string? number = Convert.ToString(Console.ReadLine());
-            // int[] arrayNumber = new int[number.Length];
-            // for (int i = 0; i < number.Length; i++)
-            // {
-            //     arrayNumber[i] = Convert.ToInt32(number[i].ToString());
-            //     // Console.WriteLine(arrayNumber[i]);
-            // }
-            // Console.WriteLine(arrayNumber[^5]);
-            // Console.WriteLine(number[3]);
-
-            if (number[0] == number[^1])
+            string? number = Convert.ToString(Console.ReadLine());   
+            for (int i = 1; i <= number.Length / 2; i++)
             {
-                if (number[01] == number[^2])
+                
+                
+                if (number[i-1] == number[^i])
                 {
-                   Console.WriteLine("Это полиндром");
-                }  
+                    if(i == number.Length/2)
+                    {
+                        Console.WriteLine("Это полиндром");
+                    }
+                    
+                    
+                }
+                else 
+                {
+                    Console.WriteLine("Это не полиндром");
+                    break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Это не полиндром");
-            }
-            // int j = 1;
-            // while (j < number.Length)
-            // {
-            //     if (arrayNumber[j-1] == number[^j])
-            //     {
-            //         j++;
-            //     }
-            //     else
-            //     {
-            //         break;
-            //         Console.WriteLine();
-            //     }
-            // }
+            
 
         }
     }
 }
+
+//  int[] arrayNumber = new int[number.Length]; не убираю чтобы сохранить!!!
+//             for (int i = 0; i < number.Length; i++)
+//             {
+//                 arrayNumber[i] = Convert.ToInt32(number[i].ToString());
+//                 // Console.WriteLine(arrayNumber[i]);
+//             }
